fd10ffa2d6f65bc4d97316e75a59c75350a56e43
ntan1902 <trinh.an.1902@gmail.com>
2021-05-20 14:37:42 +0700

    Clean folder Exercise

--- demo/src/main/java/com/vng/demo/DemoApplication.java
+++ demo/src/main/java/com/vng/demo/DemoApplication.java
@@ -2,12 +2,24 @@ package com.vng.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Controller
+@RequestMapping
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@ResponseBody
+	@GetMapping("/")
+	public String hello() {
+		return "Hello Spring";
+	}
 }
